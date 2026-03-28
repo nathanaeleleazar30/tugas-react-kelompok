5cc072517bbfa3685a77d37baf0e9334038647d0
DeltaBinery <rrhn.allam@gmail.com>
2026-03-29 00:19:04 +0700

    menambahkan profil raihan

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ProfileNathanael from './ProfileNathanael';
+import ProfileRaihan from './ProfileRaihan'; // <-- 1. Ini tambahan import untuk profilmu
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Jika buka localhost:3000 biasa, arahkan ke halaman profilmu */}
+        {/* Jika buka localhost:3000 biasa, arahkan ke halaman profil ketua */}
         <Route path="/" element={<Navigate to="/nathanael" replace />} />
         
-        {/* URL spesifik untuk halamanmu */}
+        {/* URL spesifik untuk halaman Nathanael */}
         <Route path="/nathanael" element={<ProfileNathanael />} />
+        
+        {/* URL spesifik untuk halamanmu (Raihan) */}
+        <Route path="/raihan" element={<ProfileRaihan />} /> {/* <-- 2. Ini tambahan rute untukmu */}
         
         {/* Nanti temanmu bisa tambahkan route mereka di sini */}
         {/* <Route path="/surya" element={<ProfileSurya />} /> */}
