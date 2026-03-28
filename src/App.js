5fa2cf61aef811c73f2825ba00d8665d95d9f5a2
Starlumee <bintangjemjem2@gmail.com>
2026-03-29 02:32:18 +0700

    menambahkan fitur bagian bintang

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import ProfileNathanael from './ProfileNathanael';
+import ProfileBintang from './ProfileBintang';
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Jika buka localhost:3000 biasa, arahkan ke halaman profilmu */}
-        <Route path="/" element={<Navigate to="/nathanael" replace />} />
+        <Route path="/" element={<Navigate to="/bintang" replace />} />
         
         {/* URL spesifik untuk halamanmu */}
-        <Route path="/nathanael" element={<ProfileNathanael />} />
+        <Route path="/bintang" element={<ProfileBintang />} />
         
         {/* Nanti temanmu bisa tambahkan route mereka di sini */}
         {/* <Route path="/surya" element={<ProfileSurya />} /> */}
